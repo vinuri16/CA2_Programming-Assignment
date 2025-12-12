556580f77006ae40ed56b22002eb3280a3868c03
vinuri16 <20067110@mydbs.ie>
2025-12-12 00:07:46 +0000

    created plantRoutes.js

---
+++ routes/plantRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const plantController = require('../controllers/plantController');
+
+// GET all plants
+router.get('/', plantController.getAllPlants);
+
+// GET a single plant by ID
+router.get('/:id', plantController.getPlantById);
+
+// POST a new plant
+router.post('/', plantController.createPlant);
+
+// PUT (update) a plant
+router.put('/:id', plantController.updatePlant);
+
+// DELETE a plant
+router.delete('/:id', plantController.deletePlant);
+
+module.exports = router;
